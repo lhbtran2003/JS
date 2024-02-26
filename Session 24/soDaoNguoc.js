636a9c94e8510a577bfe0f04cde7d22d26acb0fe
lhbtran2003 <lhbtran2003@gmail.com>
2024-02-26 09:38:09 +0700

    commit

---
+++ Session 24/soDaoNguoc.js
@@ -0,0 +1,10 @@
+document.getElementById("mySubmit").onclick = function(){
+    let number = Number(document.getElementById("theNumber").value);
+    let reverse = 0;
+    while (number > 0) {
+        reverse = number%10 + reverse*10 ;
+        number = Math.floor(number / 10);
+    }
+    console.log(reverse);
+    document.getElementById("myH1").textContent = 'Số đảo ngược là ' + reverse + ' đó quý vị';
+}
