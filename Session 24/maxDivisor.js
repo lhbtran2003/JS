c0b0f8a445644dc178ced184b7fe0385f9fe88a6
lhbtran2003 <lhbtran2003@gmail.com>
2024-02-29 06:58:19 +0700

    commit

---
+++ Session 24/maxDivisor.js
@@ -0,0 +1,4 @@
+let a = Number(prompt(`Nhập số nguyên dương a:`));
+let b = Number(prompt(`Nhập số nguyên dương b:`));
+let c = Number(prompt(`Nhập số nguyên dương c:`));
+let d = Number(prompt(`Nhập số nguyên dương d:`));
